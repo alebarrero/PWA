4cb67a0b4232b8052d299fc36dbdb40319030efa
alebarrero <72764436+alebarrero@users.noreply.github.com>
2023-05-30 11:35:07 -0400

    Add files via upload

---
+++ Develop/client/src/js/database.js
@@ -0,0 +1,39 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) =>{
+  console.log('update the jate database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const req = store.put({ id: 1, value: content });
+  const res = await req;
+  console.log('Data saved to the jate database', res);
+};
+
+// TODO: Add logic for a method that gets all the content from the database
+export const getDb = async () =>
+{
+  console.log('GET from the jate database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const req = store.getAll();
+  const res = await req;
+  console.log('result.value', res);
+  return res?.value;
+};
+
+initdb();
---
+++ README.md
@@ -0,0 +1,9 @@
+# J.A.T.E - Just Another Text Editor 
+
+This minimal text editor to create and keep track of notes! While the application is hosted online, run the install from the webpage in order to use this text editor offline.
+
+# Installation
+Install node package NPM I
+Run NPM START
+Start App with the guidelines
+
